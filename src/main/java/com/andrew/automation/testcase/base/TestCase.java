473df4f8cb50952c14d8f270a5ece730d11457d5
Andrew Yun <andrew.yun@seraitrade.com>
2021-09-09 12:59:31 +0800

    add default timeout 60second and functions waitElementToBeSelected, waitElementsIsVisible

--- src/main/java/com/andrew/automation/testcase/base/TestCase.java
+++ src/main/java/com/andrew/automation/testcase/base/TestCase.java
@@ -19,6 +19,8 @@ public abstract class TestCase {
 
     private Map<Class, Object> pages = new HashMap<Class, Object>();
 
+    private int defaultTimeout = 60; //Default Timeout 60 Seconds
+
     public <P extends Page> P page(Class<P> pageClass) throws Exception {
         if(!Page.class.isAssignableFrom(pageClass)){
             throw new Exception("Class no extends Page");
@@ -34,8 +36,23 @@ public abstract class TestCase {
     }
 
     public void waitElementToBeClickable(WebElement element) {
-        WebDriverWait wait = new WebDriverWait(driver, 600);
+        WebDriverWait wait = new WebDriverWait(driver, defaultTimeout);
         wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public void waitElementToBeSelected(WebElement element) {
+        WebDriverWait wait = new WebDriverWait(driver, defaultTimeout);
+        wait.until(ExpectedConditions.elementToBeSelected(element));
+    }
+
+
+    public void waitElementsIsVisible(WebElement... elements) {
+        waitElementsIsVisible(defaultTimeout, elements);
+    }
+
+    public void waitElementsIsVisible(int timeoutSecond, WebElement... elements) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutSecond);
+        wait.until(ExpectedConditions.visibilityOfAllElements(elements));
     }
 
 }
